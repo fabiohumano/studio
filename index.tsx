a3f56395ed9e036ecf33adeb5f576f86057e006e
fabiohumano <humanoprojeto@gmail.com>
2025-12-20 23:58:35 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,17 +1,15 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
+const container = document.getElementById('root');
 
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
+if (container) {
+  const root = createRoot(container);
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
-} else {
-  console.error("Erro crítico: Elemento #root não encontrado no DOM.");
 }
